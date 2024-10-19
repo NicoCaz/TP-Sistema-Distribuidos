0ef34152b8724a4efa1f5a565b717157ecc2772c
NicoCaz <nicolascazorlamartinez@gmail.com>
2024-10-19 15:43:23 -0300

    alguna vez les dije que odio JS?

--- SPA/public/pages/home.js
+++
@@ -1,6 +0,0 @@
-function homePage() {
-    return `
-<h1>Bienvenido al increible front hecho con dos pesos</h1>
-<p>Usa la nanvar para ir a los otros lados pa.</p>
-    `;
-}
--- SPA/public/pages/puntosDeControl.js
+++
@@ -1,6 +0,0 @@
-function puntosDeControlPage() {
-    return `
-<h1>Aca van los puntos de control</h1>
-<p>aca poner una lista y despues el formulario</p>
-    `;
-}
