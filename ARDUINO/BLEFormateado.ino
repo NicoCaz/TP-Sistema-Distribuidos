80c6eb44af879c32816471841812c3db255eddce
Nicolás Boolls <nicolasboolls@gmail.com>
2024-11-11 18:08:32 -0300

    Envio de paquetes WEMOS
    Envío de datos mediante MQTT formateado para que envíe mediante paquetes xq con muchos dispositivos se rompía.

--- ARDUINO/BLEFormateado.ino
+++ ARDUINO/BLEFormateado.ino
@@ -3,6 +3,7 @@
 #include <BLEDevice.h>
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
+#include <ArduinoJson.h>
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -12,109 +13,112 @@ const char* password = "chesterton123";  // xD
 const char* mqttServer = "192.168.0.4"; 
 const int mqttPort = 1883; 
 String macWemos;
-const char* topic = "test/topic"; // El topic donde se publicará la lista de dispositivos
+int packageNum = 1;
+int totalPackages = 1;
+int paquetesMaximos = 2;
 
-unsigned long previousMillis = 0; // Almacena el último tiempo de ejecución
-const long interval = 5000; // Intervalo de tiempo en milisegundos (5 segundos)
+std::vector<std::string> deviceList;
+const char* topic = "test/topic"; 
+
+unsigned long previousMillis = 0; 
+const long interval = 5000; 
 
 BLEScan* pBLEScan;
 
-/*class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
-  void onResult(BLEAdvertisedDevice advertisedDevice) {
-    Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
-
-    // publicar la direccion del dispositivo en MQTT
-    String deviceAddress = advertisedDevice.getAddress().toString().c_str();
-    if (client.publish(topic, deviceAddress.c_str())) {
-      Serial.print("Dispositivo BLE enviado a MQTT: ");
-      Serial.println(deviceAddress);
-    } else {
-      Serial.println("Error al publicar en MQTT");
-    }
-  }
-}; */
 
 void setup() {
   Serial.begin(115200); 
-  
   connectToWiFi();
   connectToMQTT();
-
   client.setServer(IPAddress(192, 168, 0, 4), mqttPort);
-
-  // Inicializar BLE
-  BLEDevice::init("ESP32_BLE"); // Nombre del dispositivo BLE
-  pBLEScan = BLEDevice::getScan(); // Crear el escáner BLE
- // pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(true); // Escaneo activo
-  pBLEScan->setInterval(100); // Intervalo de escaneo
-  pBLEScan->setWindow(99); // Ventana de escaneo
+  BLEDevice::init("ESP32_BLE"); 
+  pBLEScan = BLEDevice::getScan(); 
+  pBLEScan->setActiveScan(true); 
+  pBLEScan->setInterval(100); 
+  pBLEScan->setWindow(99); 
 }
 
 void loop() {
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("Desconectado del Wi-Fi, intentando reconectar...");
-    connectToWiFi(); // Intenta reconectar
+    connectToWiFi(); 
   } else {
-   /* Serial.println("Conexión Wi-Fi activa.");
+    /* Serial.println("Conexión Wi-Fi activa.");
     Serial.print("SSID: ");
     Serial.println(WiFi.SSID());
     Serial.print("Dirección IP: ");
     Serial.println(WiFi.localIP()); */
-  } 
-
+  }
 
   if (!client.connected()) {
     Serial.println("Desconectado del MQTT, intentando reconectar...");
-    connectToMQTT(); // Intenta reconectar al MQTT
+    connectToMQTT(); 
   } else {
-    client.loop(); 
-    unsigned long currentMillis = millis();  
-      if (currentMillis - previousMillis >= interval) {   
-        previousMillis = currentMillis; 
+    client.loop();
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
 
-        // Escanear dispositivos BLE cercanos
-        Serial.println("Buscando dispositivos BLE cercanos...");
-        BLEScanResults * scanResults = pBLEScan->start(5, false); // Escanear durante 5 segundos
-        pBLEScan->stop(); 
+      
+      Serial.println("Buscando dispositivos BLE cercanos...");
+      BLEScanResults* foundDevices = pBLEScan->start(5); 
 
+      int deviceCount = foundDevices->getCount();
 
-        String jsonData = "{ \"checkpointID\": \"" + macWemos + "\", \"animals\": [";
+     
+      int packageNum = 1;
+      int totalPackages = (deviceCount / paquetesMaximos) + 1;
 
-        // Mostrar la cantidad de dispositivos BLE encontrados
-        Serial.println("Cantidad de cosos escaneados:");
-        Serial.println(scanResults->getCount());
+      
+      for (int i = 0; i < deviceCount; i++) {
+        BLEAdvertisedDevice device = foundDevices->getDevice(i);
+        String macAddress = device.getAddress().toString().c_str();
+        int rssi = device.getRSSI();
 
-        // Iterar sobre los dispositivos BLE escaneados
-        for (int i = 0; i < scanResults->getCount(); i++) {
-          BLEAdvertisedDevice device = scanResults->getDevice(i);
-          String id = device.getAddress().toString().c_str();
-          int rssi = device.getRSSI();
+        
+        if (i % paquetesMaximos == 0) {
+          
+          DynamicJsonDocument doc(256); 
+          doc["packageNum"] = packageNum;
+          doc["totalPackages"] = totalPackages;
+          doc["checkpointID"] = macWemos;
 
-          // Añadir cada dispositivo al JSON
-          jsonData += "{ \"id\": \"" + id + "\", \"rssi\": " + String(rssi) + " }";
+          JsonArray animals = doc.createNestedArray("animals");
 
-          // Añadir coma entre dispositivos, excepto después del último
-          if (i < scanResults->getCount() - 1) {
-            jsonData += ", ";  // Añadir coma entre elementos
+          
+          for (int j = i; j < min(i + paquetesMaximos, deviceCount); j++) {
+            BLEAdvertisedDevice device = foundDevices->getDevice(j);
+            String macAddress = device.getAddress().toString().c_str();
+            int rssi = device.getRSSI();
+
+            JsonObject animal = animals.createNestedObject();
+            animal["id"] = macAddress;
+            animal["rssi"] = rssi;
           }
+
+          
+          String jsonString;
+          serializeJson(doc, jsonString);
+          Serial.println("Enviando paquete:");
+          Serial.println(jsonString);  
+          if (client.publish(topic, jsonString.c_str())) {
+             Serial.println("Datos enviados al servidor MQTT");
+          } else {
+             Serial.println("Error al publicar en MQTT");
+         }
+
+          
+          packageNum++;
         }
-
-        // Cerrar el array y el objeto JSON
-        jsonData += "] }";
-      Serial.println("Datos escaneados en JSON:");
-      Serial.println(jsonData); // Imprimir el JSON en el formato deseado
-
-        if (client.publish(topic, jsonData.c_str())) {
-        Serial.println("Datos enviados al servidor MQTT");
-      } else {
-        Serial.println("Error al publicar en MQTT");
       }
-      }
+
+      delay(10000); 
+    }
   }
-
-  
 }
+  
+
+
 
 void connectToWiFi() {
   Serial.print("Conectando a WiFi...");
@@ -135,16 +139,13 @@ void connectToWiFi() {
 }
 
 void connectToMQTT() {
-  //while (!client.connected()) {
     Serial.print("Intentando conectar a MQTT...");
-    // Intentar conectarse al servidor MQTT
     if (client.connect("ESP32Client")) { 
       Serial.println("conectado");
     } else {
       Serial.print("Error de conexión, rc=");
       Serial.print(client.state());
       Serial.println(" Intentando de nuevo en 5 segundos...");
-      delay(5000); // Esperar 5 segundos antes de volver a intentar
+      delay(5000); 
     }
- // }
 }
