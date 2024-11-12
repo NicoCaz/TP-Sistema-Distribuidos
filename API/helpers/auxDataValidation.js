07544377f1a3a0c006912aeccc62bfb827ca856a
kevin5845 <kzamora@alumnos.fi.mdp.edu.ar>
2024-11-12 09:20:45 -0300

    arreglo de la validacion CP

--- API/helpers/auxDataValidation.js
+++ API/helpers/auxDataValidation.js
@@ -5,13 +5,14 @@ const filePath = path.join(__dirname, '..', 'BBDD','checkpoints.json');
 
 const getCheckpoint = () => {
   const data = fs.readFileSync(filePath, 'utf8');
-  return JSON.parse(data);
+  const jsonData = JSON.parse(data);
+  return jsonData.checkpoints;
 };
 
 const valCheckpoint = (checkpointID) => {
- // const regCheckpoint = getCheckpoint();
-  //const check = regCheckpoint.find((check) => check.checkpointID === checkpointID);
- // if (!check) throw new Error("401");
+  const regCheckpoint = getCheckpoint();
+  const check = regCheckpoint.find((check) => check.id === checkpointID);
+  if (!check) throw new Error("401");
   return true;
 };
 
