3bbb1819d16bbbadbbe0690a4812e457600ffbf3
kevin5845 <kzamora@alumnos.fi.mdp.edu.ar>
2024-11-12 12:40:53 -0300

    cambio menor

--- API/routes/availableDevices.js
+++ API/routes/availableDevices.js
@@ -1,16 +1,41 @@
 const { getCompletedSMS } = require('../checkpointManager');
 
 
+
 const availableDevices = (req, res) => {//ver como actualizar para tener los datos de los dispositivos receintes
   const completedData = getCompletedSMS(); 
+  let aux = {
+    packageNum: 1,
+    checkpointID: '12345',
+    totalPackages: 1,
+    contPackages: 1,
+    animals: [
+      { id: 'animal1', rssi: -77 },
+      { id: 'animal2', rssi: -44 },
+      { id: 'animal3', rssi: -81 }
+    ]
+  };
+
 
   if (req.url === '/api/availableDevices' && req.method === 'GET') {
- 
-    if (completedData) {
-        res.json(completedData.animals); //array de animales
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    if (true/*completedData*/) {
+        res.end(JSON.stringify(aux.animals));; //array de animales
+
+       // res.end(JSON.stringify(completedData.animals));; //array de animales
     } else {
-        res.status(404).json({ message: 'No se encontraron dispositivos cerca' });
-    }
-}};
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'No se encontraron dispositivos cerca' }));
+      }
+} else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Ruta no encontrada' }));
+  }
+
+
+
+
+};
 
 module.exports = availableDevices;
