c09c6bf9e6e9bd2446e25ae67925bd4e7fe0f7bc
NicoCaz <nicolascazorlamartinez@gmail.com>
2024-11-13 20:47:16 -0300

    Agregar registros de consola para depuración en availableDevices y actualizar la URL base en AnimalPositionService

--- API/routes/availableDevices.js
+++ API/routes/availableDevices.js
@@ -1,28 +1,29 @@
 const { getCompletedSMS: getCompletedListDevices } = require('../checkpointManager');
 
-
-
-const availableDevices = (req, res) => {//ver como actualizar para tener los datos de los dispositivos receintes
-  const completedData = getCompletedListDevices(); 
+const availableDevices = (req, res) => {
+  console.log(`\n🔄 Nueva solicitud: ${req.method} ${req.url}`);
   
-
+  console.log('📂 Intentando obtener lista de dispositivos completados');
+  const completedData = getCompletedListDevices();
+  
   if (req.url === '/api/availableDevices' && req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-
+    console.log('📱 Procesando solicitud de dispositivos disponibles');
+    
     if (completedData) {
-        res.end(JSON.stringify(completedData));; //array de animales
+      console.log('✅ Datos de dispositivos encontrados');
+      console.log('📤 Enviando lista de dispositivos:', completedData);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(completedData));
     } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'No se encontraron dispositivos cerca' }));
-      }
-} else {
+      console.log('❌ No se encontraron dispositivos cerca');
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'No se encontraron dispositivos cerca' }));
+    }
+  } else {
+    console.log('❌ Ruta no encontrada:', req.url);
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Ruta no encontrada' }));
   }
-
-
-
-
 };
 
 module.exports = availableDevices;
--- SPA/js/services/AnimalPositionService.js
+++ SPA/js/services/AnimalPositionService.js
@@ -1,11 +1,14 @@
 export class AnimalPositionService {
     constructor() {
-        this.baseUrl = '/api/animals/position';
+        this.baseUrl = 'http://localhost:3000/api';
     }
 
     async getAnimalPositions() {
         try {
-            const response = await fetch(this.baseUrl);
+            console.log('Obteniendo posiciones de animales...');
+            console.log('URL:', this.baseUrl);
+            const response = await fetch(`${this.baseUrl}/animals/position`);
+            console.log('Response:', response);
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
