fce7bb6f26bac901aee998bd8b592aef3354f10b
NicoCaz <nicolascazorlamartinez@gmail.com>
2024-11-14 23:23:39 -0300

    prueba deploy

--- SPA/js/config.js
+++ SPA/js/config.js
@@ -1,3 +1,6 @@
 export const config = {
-    API_URL: 'http://localhost:3000/API'
+   // API_URL: 'http://localhost:3000/API'
+   API_URL: 'http://vwo0cowwgowckco80kcwgs84.173.208.151.236.sslip.io/API'
 };
+
+
